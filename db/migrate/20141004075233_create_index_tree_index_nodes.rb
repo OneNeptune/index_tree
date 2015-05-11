84f001149e06f23c7fa001a932175d19923b00c0
Marek Pietrucha <marek@enginearch.com>
2015-05-11 23:45:24 +0200

    changed integer ids to uuids

--- db/migrate/20141004075233_create_index_tree_index_nodes.rb
+++ db/migrate/20141004075233_create_index_tree_index_nodes.rb
@@ -1,8 +1,12 @@
 class CreateIndexTreeIndexNodes < ActiveRecord::Migration
   def change
-    create_table :index_tree_index_nodes do |t|
-      t.references :root_element, polymorphic: true, :null => false
-      t.references :node_element, polymorphic: true, :null => false
+    create_table :index_tree_index_nodes, id: :uuid do |t|
+      t.uuid :root_element_id, :null => false
+      t.string :root_element_type, :null => false
+
+      t.uuid :node_element_id, :null => false
+      t.string :node_element_type, :null => false
+
       t.timestamps
     end
   end
